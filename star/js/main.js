d6448ee57c3fbb41e498442d31c29665d487eeab
wuling31715 <wuling31715@gmail.com>
2021-01-13 18:15:46 +0800

    .

--- star/js/main.js
+++ star/js/main.js
@@ -2,3 +2,18 @@ function repeat_password () {
   var password = document.getElementById("password").value;
   document.getElementById("password2").value = password;        
 }
+
+function delete_equipment(eid) {
+  var input_value = document.getElementById("input_value").value = eid;
+  document.getElementById("submit_button").click();  
+}
+
+function project_action (action) {
+  var input_value = document.getElementById("input_value").value = action;
+  document.getElementById("submit_button").click();
+}
+
+function delete_project (pid) {
+  var input_value = document.getElementById("input_value").value = pid;
+  document.getElementById("submit_button").click();
+}
