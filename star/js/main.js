f4d47ed733dea7ad1172766c50634c0b39ef9054
wuling31715 <wuling31715@gmail.com>
2021-01-14 16:03:47 +0800

    .

--- star/js/main.js
+++ star/js/main.js
@@ -17,3 +17,33 @@ function delete_project (pid) {
   var input_value = document.getElementById("input_value").value = pid;
   document.getElementById("submit_button").click();
 }
+
+function change_progress_bar () {
+  var progress = document.getElementById("progress").value;
+  var progress_bar = document.getElementById("progress_bar");
+  var percentage = progress.concat("%");
+  progress_bar.style.width = percentage;
+  progress_bar.innerHTML = percentage;
+}
+
+function add_target_table () {
+  var table = document.getElementById('target_table');
+  var row = table.insertRow(-1);
+  var cell0 = row.insertCell(0);
+  var cell1 = row.insertCell(1);
+  var cell2 = row.insertCell(1);
+  cell0.innerHTML = '<input type="text"  class="form-control" id="target_name">';
+  cell1.innerHTML = '<input type="number" step="0.01"  class="form-control" id="ra">';
+  cell2.innerHTML = '<input type="number" step="0.01"  class="form-control" id="dec">';
+}
+
+function add_participant_table () {
+  var table = document.getElementById('participant_table');
+  var row = table.insertRow(-1);
+  var cell0 = row.insertCell(0);
+  var cell1 = row.insertCell(1);
+  var cell2 = row.insertCell(1);
+  cell0.innerHTML = '<input type="text"  class="form-control" id="participant_uid">';
+  cell1.innerHTML = '<input type="text"   class="form-control" id="participant_name">';
+  cell2.innerHTML = '<input type="email"   class="form-control" id="participant_email">';
+}
